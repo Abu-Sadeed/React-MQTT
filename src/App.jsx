25b7b0b6cc555d71350f55fd0672478e3a17113d
Sadeed/ITP/Md. Abu Sadeed (Email: abu.sadeed@reddotdigitalit.com) <abu.sadeed@reddotdigitalit.com>
2023-07-31 07:16:34 +0600

    Initialized testing with mqtt and react

---
+++ src/App.jsx
@@ -0,0 +1,42 @@
+import React, { useState, useEffect } from "react";
+import mqtt from "precompiled-mqtt";
+
+const App = () => {
+  const [liveData, setLiveData] = useState("");
+  const option = {
+    username: "reddot_smartlock",
+    password: "reddotsmrtlock@kitenco%2023",
+  };
+  useEffect(() => {
+    // Replace 'mqtt://your-broker-url' with your actual broker URL
+    const brokerUrl = "mqtt://202.134.12.131:15575";
+    const client = mqtt.connect(brokerUrl, option);
+
+    // Subscribe to the topic you want to receive live data from
+    const topic = "your-topic";
+    client.subscribe(topic);
+
+    // Handle incoming messages
+    client.on("message", (receivedTopic, message) => {
+      if (receivedTopic === topic) {
+        const data = message.toString();
+        setLiveData(data);
+      }
+    });
+
+    // Clean up the MQTT client on component unmount
+    return () => {
+      client.unsubscribe(topic);
+      client.end();
+    };
+  }, []);
+
+  return (
+    <div>
+      <h2>Live Data from MQTT:</h2>
+      <p>{liveData}</p>
+    </div>
+  );
+};
+
+export default App;
